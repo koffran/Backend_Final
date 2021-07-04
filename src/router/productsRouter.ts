c1642e463f8bc3bdf9428221649056c254a80024
Franco <fran.e.zarate@gmail.com>
2021-07-04 16:16:22 -0300

    create admin

--- src/router/productsRouter.ts
+++ src/router/productsRouter.ts
@@ -4,32 +4,35 @@ const router = express.Router();
 import Product from '../service/product'
 import {ProductsService} from '../service/productos.service';
 export const productsService = new ProductsService();
+const admin = false;
 
 /**
  * Permite listar todos los productos o uno por su ID
  * disponible para usuarios y administradores
  */
-router.get('/:id?', (req,res) =>{
-
-    if(req.params.id === undefined){
-         res.json(productsService.getAllProducts())
-    }
-    else{
-        try {
-            res.json(productsService.getProductById(parseInt(req.params.id)))
-        } catch (error) {
-            res.status(404).send(error.message)
-            
+router.get('/:id?',(req,res) =>{
+    try {
+        if(req.params.id === undefined){
+            res.json(productsService.getAllProducts())
         }
-
-    }
+        else{
+        res.json(productsService.getProductById(parseInt(req.params.id)))
+        }
+    } catch (error) {
+        res.status(404).send(error.message)
+        
+    }    
 })
+
 
 /**
  * Incorpora productos al listado
  * Solo administradores
  */
-router.post('/', (req, res)=>{
+router.post('/',(req,res,next)=>{
+    admin?next():res.send({"error":-1, "descripcion": `ruta ${req.path} metodo ${req.method} no autorizada`})
+},
+(req, res)=>{
    try {
         productsService.createProduct(req.body);
         res.sendStatus(201)   
@@ -44,7 +47,9 @@ router.post('/', (req, res)=>{
  * Actualiza un producto por ID
  * Solo administradores
  */
-router.patch('/:id', (req,res)=>{
+router.patch('/:id',(req,res,next)=>{
+    admin?next():res.send({"error":-1, "descripcion": `ruta ${req.path} metodo ${req.method} no autorizada`})
+}, (req,res)=>{
     try {
         productsService.updateProductById(req)
          res.sendStatus(204);   
@@ -59,7 +64,9 @@ router.patch('/:id', (req,res)=>{
  * Borra un producto por ID
  * Solo administradores.
  */
-router.delete('/:id',(req,res)=>{
+router.delete('/:id',(req,res,next)=>{
+    admin?next():res.send({"error":-1, "descripcion": `ruta ${req.path} metodo ${req.method} no autorizada`})
+},(req,res)=>{
     try {
         productsService.deleteProductById(parseInt(req.params.id))
         res.sendStatus(200);  
